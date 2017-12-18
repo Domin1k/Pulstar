0f19594d8fc86c68a3df4052420911773cee22bb
Domin1k <kristian.lubenov@gmail.com>
2017-12-18 23:02:52 +0200

    Removed fully hard coded image  links

---
+++ src/Pulstar/Pulstar.Web/Extensions/RazorExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Pulstar.Web.Extensions
+{
+    using System;
+
+    public static class RazorExtensions
+    {
+        public static string ToBase64(this byte[] image)
+        {
+            var base64 = Convert.ToBase64String(image);
+            var imgSrc = string.Format("data:image/gif;base64,{0}", base64);
+            return imgSrc;
+        }
+    }
+}
